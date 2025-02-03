f1a7d4a8a5436d5c702b10a51e01312963ce5b21
LiliiaVersta <versta270695@gmail.com>
2025-02-03 16:46:54 +0100

    fix

--- dictionary/task_3.py
+++ dictionary/task_3.py
@@ -4,7 +4,7 @@ books = {
     "book1": {"title": "1984", "author": "George Orwell", "year": 1949},
     "book2": {"title": "To Kill a Mockingbird", "author": "Harper Lee", "year": 1960}
 }
-for book_key, book_info in books.items():
+for book_key, book_info in books.values():
     title = book_info["title"]
     author = book_info["author"]
     year = book_info["year"]
