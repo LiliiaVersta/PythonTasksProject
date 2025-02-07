066c3ac53b7ff58994c5ea0c23a5974fd15a35cd
LiliiaVersta <versta270695@gmail.com>
2025-02-07 17:51:46 +0100

    OOP

---
+++ oop/task_2.py
@@ -0,0 +1,9 @@
+class Rectangle:
+    def __init__(self, width, height):
+        self.width = width    
+        self.height = height  
+
+rect = Rectangle(5, 10)
+
+print("Width:", rect.width)
+print("Height:", rect.height)
