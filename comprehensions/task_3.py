df0130332dfc2cbf421b498293d809304c7afdf2
LiliiaVersta <versta270695@gmail.com>
2025-02-19 19:19:59 +0100

    add comprehensions

---
+++ comprehensions/task_3.py
@@ -0,0 +1,4 @@
+# Создаём словарь, где ключ — число, а значение — его квадрат
+squares_dict = {x: x**2 for x in range(1, 6)}
+
+print(squares_dict)
---
+++ comprehensions/task_4.py
@@ -0,0 +1,4 @@
+nested_list = [[1, 2, 3], [4, 5], [6, 7]]
+
+flattened_list = sum(nested_list, []) # sum() для объединения списков
+print(flattened_list)
